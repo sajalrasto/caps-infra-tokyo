8352ef83ef213c41a59f31379a4f25084f056a42
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-18 16:46:29 +0530

    Create route.tf

---
+++ route.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "terraform-public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+
+  tags = {
+    Name = "${var.Main_Routing_Table}"
+  }
+}
