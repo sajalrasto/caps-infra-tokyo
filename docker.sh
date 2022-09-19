6bfc96ae10d30e13c91b52c8b519c9d9bf28d18f
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-19 16:44:52 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+sudo yum -y update
 sudo amazon-linux-extras install java-openjdk11 -y
-sudo amazon-linux-extras install docker -y
 sudo yum install docker -y
-sudo service docker start
-sudo service docker enable
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 #Make docker auto-start
 
@@ -16,24 +16,26 @@ sudo chmod 666 /var/run/docker.sock
 #Because you always need it....
 
 sudo yum install -y git
-
 #Reboot to verify it all loads fine on its own.
 
 #sudo reboot
 
-docker-compose install
 #Copy the appropriate docker-compose binary from GitHub:
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 #NOTE: to get the latest version (thanks @spodnet): 
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
+#sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+#sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+#sudo chmod -v +x /usr/local/bin/docker-compose
 #Fix permissions after download:
 
-sudo chmod +x /usr/local/bin/docker-compose
-
+#sudo chmod +x /usr/local/bin/docker-compose
 #Verify success:
 
 docker-compose version
-sudo amazon-linux-extras install java-openjdk11 -y
+
