27d4d11eeea1156edf1bd2eeba17411221cea5d1
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-19 13:35:42 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo yum update –y
+sudo yum install -y git
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 #sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
