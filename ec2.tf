56e1c17ef351e1a500d058c7b77d73c715010c7d
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-19 13:44:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,19 +9,19 @@ resource "aws_instance" "instance_type" {
   user_data                   = "${file("jenkins.sh")}"
   key_name                    = var.key_name
   tags = {
-    Name = "jnekins-master"
+    Name = "jenkins-master"
   }
 }
 
 resource "aws_instance" "instance_type" {
   ami                         = "ami-05fa00d4c63e32376"
-  instance_type               = var.instance_type
+  instance_type               = "t2.xlarge"
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   subnet_id                   = aws_subnet.subnet1-public.id
   associate_public_ip_address = true
   availability_zone           = var.azs
   #user_data                   = file("docker.sh")
-  user_data                   = "${file("jenkins.sh")}"
+  #user_data                   = "${file("jenkins.sh")}"
   key_name                    = var.key_name
   tags = {
     Name = "stagging"
