0924632deecbde0c55605fe2d796aec4d6d8c4f4
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-19 14:31:42 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-sudo yum update –y
+sudo yum update -y
 sudo yum install -y git
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum upgrade
-sudo yum install jenkins
-sudo amazon-linux-extras install java-openjdk11
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum upgrade -y
+sudo yum install jenkins -y
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
+sudo systemctl enable jenkins
 sudo systemctl status jenkins
 cat /var/lib/jenkins/secrets/initialAdminPassword
