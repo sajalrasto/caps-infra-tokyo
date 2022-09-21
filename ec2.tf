cbdb99808f89b9117b44083c08bb496824d6b275
Varaha Lakshmi Manoj Kandala <93027975+k-Manoj@users.noreply.github.com>
2022-09-21 11:49:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -32,3 +32,32 @@ resource "aws_instance" "instance_type1" {
     Name = "stagging"
   }
 }
+
+resource "aws_instance" "instance_type2" {
+  ami                         = "ami-05fa00d4c63e32376"
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  subnet_id                   = aws_subnet.subnet1-public.id
+  associate_public_ip_address = true
+  availability_zone           = var.azs
+  #user_data                   = file("docker.sh")
+  #user_data                   = "${file("jenkins.sh")}"
+  key_name                    = var.key_name
+  tags = {
+    Name = "kubernetes-master"
+  }
+}
+resource "aws_instance" "instance_type3" {
+  ami                         = "ami-05fa00d4c63e32376"
+  instance_type               = var.instance_type
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  subnet_id                   = aws_subnet.subnet1-public.id
+  associate_public_ip_address = true
+  availability_zone           = var.azs
+  #user_data                   = file("docker.sh")
+  #user_data                   = "${file("jenkins.sh")}"
+  key_name                    = var.key_name
+  tags = {
+    Name = "k8s-worker-node"
+  }
+}
