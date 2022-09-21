fd84a52e603206ae40945a860cda50d58c693c1d
sajalrasto <30717607+sajalrasto@users.noreply.github.com>
2022-09-21 12:18:23 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "default" {
   enable_dns_hostnames = true
   tags = {
     Name        = "${var.vpc_name}"
-    Owner       = "Manoj"
+    Owner       = "Sajal"
     environment = "${var.environment}"
   }
 }
