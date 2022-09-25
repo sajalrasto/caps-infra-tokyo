4d64da2dcc486325c0b8616f8cff527215f0f458
sajalrasto <30717607+sajalrasto@users.noreply.github.com>
2022-09-25 19:36:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance_type" {
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = "ami-078296f82eb463377"
   instance_type               = var.instance_type
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   subnet_id                   = aws_subnet.subnet1-public.id
@@ -15,7 +15,7 @@ resource "aws_instance" "instance_type" {
        delete_on_termination =true
     }
   tags = {
-    Name = "ansi-jenki-master"
+    Name = "ansi-jenki-master(sajal)"
   }
 }
 
@@ -25,13 +25,13 @@ resource "aws_eip" "lb" {
 }
 
 resource "aws_instance" "instance_type1" {
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = "ami-078296f82eb463377"
   instance_type               = var.instance_type1
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   subnet_id                   = aws_subnet.subnet1-public.id
   associate_public_ip_address = true
   availability_zone           = var.azs
-  user_data                   = file("docker.sh")
+  #user_data                   = file("docker.sh")
   #user_data                   = "${file("jenkins.sh")}"
   key_name                    = var.key_name
   root_block_device {
@@ -41,12 +41,12 @@ encrypted = true
 delete_on_termination =true
 }
   tags = {
-    Name = "stagging"
+    Name = "staging(sajal)"
   }
 }
 
 resource "aws_instance" "instance_type2" {
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = "ami-078296f82eb463377"
   instance_type               = var.instance_type
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   subnet_id                   = aws_subnet.subnet1-public.id
@@ -62,11 +62,11 @@ encrypted = true
 delete_on_termination =true
 }
   tags = {
-    Name = "kub-master"
+    Name = "kub-master(sajal)"
   }
 }
 resource "aws_instance" "instance_type3" {
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = "ami-078296f82eb463377"
   instance_type               = var.instance_type1
   vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
   subnet_id                   = aws_subnet.subnet1-public.id
@@ -82,6 +82,6 @@ encrypted = true
 delete_on_termination =true
 }
   tags = {
-    Name = "kub-worker"
+    Name = "kub-worker(sajal)"
   }
 }
